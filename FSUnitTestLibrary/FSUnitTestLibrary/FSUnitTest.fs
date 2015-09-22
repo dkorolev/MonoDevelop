8492319bf5a16611d568bafd8db30193d047a0e3
Dima <dmitry.korolev@gmail.com>
2015-09-22 12:00:51 -0700

    Added FSUnitTestLibrary

---
+++ FSUnitTestLibrary/FSUnitTestLibrary/FSUnitTest.fs
@@ -0,0 +1,16 @@
+﻿namespace FSUnitTestLibrary
+
+open System
+open NUnit.Framework
+open FsUnit
+
+[<TestFixture>]
+type Test() = 
+
+    [<Test>]
+    member x.Test1() =
+        Assert.IsTrue(true)
+
+    [<Test>]
+    member x.Test2() = 
+        1 |> should equal 1
