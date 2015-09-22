106989199d8343360aa395634ac5143d8e66a1f2
Dima <dmitry.korolev@gmail.com>
2015-09-22 14:11:23 -0700

    Added discriminated union serialization via Newton.

--- FSLibrary/SharedCode.fs
+++ FSLibrary/SharedCode.fs
@@ -3,6 +3,12 @@
 open System
 open NUnit.Framework
 open FsUnit
+open Newtonsoft.Json
+
+type Shape =
+    | Rectangle of width : float * length : float
+    | Circle of radius : float
+    | Empty
 
 type SharedFSType() = 
     static member ForCS = "F# into CS"
@@ -23,3 +29,36 @@ type Test() =
     member x.Test3() = 
         "F# into CS" |> should equal SharedFSType.ForCS
         "F# into FS" |> should equal SharedFSType.ForFS
+
+    [<Test>]
+    member x.Test4() = 
+        let shape1 = Rectangle(1.3, 10.0)
+        let shape2 = Circle(2.5)
+        let shape3 = Empty
+        let json1 = JsonConvert.SerializeObject(shape1)
+        let json2 = JsonConvert.SerializeObject(shape2)
+        let json3 = JsonConvert.SerializeObject(shape3)
+        """
+{"Case":"Rectangle","Fields":[1.3,10.0]}
+        """.Trim() |> should equal json1
+        """
+{"Case":"Circle","Fields":[2.5]}
+        """.Trim() |> should equal json2
+        """
+{"Case":"Empty"}
+        """.Trim() |> should equal json3
+        let parsed1 = JsonConvert.DeserializeObject<Shape> json1
+        let parsed2 = JsonConvert.DeserializeObject<Shape> json2
+        let parsed3 = JsonConvert.DeserializeObject<Shape> json3
+        shape1 |> should equal parsed1
+        shape2 |> should equal parsed2
+        shape3 |> should equal parsed3
+        parsed1 = shape1 |> should be True
+        parsed1 = shape2 |> should be False
+        parsed1 = shape3 |> should be False
+        parsed2 = shape1 |> should be False
+        parsed2 = shape2 |> should be True
+        parsed2 = shape3 |> should be False
+        parsed3 = shape1 |> should be False
+        parsed3 = shape2 |> should be False
+        parsed3 = shape3 |> should be True
