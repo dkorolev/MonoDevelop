468e3dde78d6b73305ed33e8da7852f974cbd51e
Dima <dmitry.korolev@gmail.com>
2015-09-22 12:39:14 -0700

    F# unit tests pass.

--- FSLibrary/SharedCode.fs
+++ FSLibrary/SharedCode.fs
@@ -1,5 +1,25 @@
 ﻿namespace SharedCodeFS
 
+open System
+open NUnit.Framework
+open FsUnit
+
 type SharedFSType() = 
     static member ForCS = "F# into CS"
     static member ForFS = "F# into FS"
+
+[<TestFixture>]
+type Test() = 
+
+    [<Test>]
+    member x.Test1() =
+        Assert.IsTrue(true)
+
+    [<Test>]
+    member x.Test2() = 
+        1 |> should equal 1
+
+    [<Test>]
+    member x.Test3() = 
+        "F# into CS" |> should equal SharedFSType.ForCS
+        "F# into FS" |> should equal SharedFSType.ForFS
